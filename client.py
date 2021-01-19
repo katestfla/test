5b4adca77b22039258f8b5bd430d82240632a342
katestfla <45404089+katestfla@users.noreply.github.com>
2021-01-18 22:00:28 -0800

    Update client.py

--- client.py
+++ client.py
@@ -197,7 +197,7 @@ class ClientHandler(BaseHandler):
 
 
 class AsyncClientHandler(BaseHandler):
-    """An async version of ClientHandler."""
+    """AsyncClientHandler is a handler of AsyncClient."""
     def __init__(self, enforce_csrf_checks=True, *args, **kwargs):
         self.enforce_csrf_checks = enforce_csrf_checks
         super().__init__(*args, **kwargs)
